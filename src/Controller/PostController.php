450e23a342dddf0f047ae48cb61cedb374f1485d
ndiaye_b <ndiaye_b@etna-alternance.net>
2019-06-12 13:25:08 +0200

    master

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Entity\PostLike;
+use App\Entity\User;
+use App\Repository\PostLikeRepository;
 use App\Repository\PostRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,4 +22,55 @@ class PostController extends AbstractController
             'posts' => $repo->findAll(),
         ]);
     }
+
+    /**
+     * @Route("/post/{id}/like", name="post_like")
+     * @param Post $post
+     * @param ObjectManager $manager
+     * @param PostLikeRepository $likeRepo
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+
+    public function like(Post $post, ObjectManager $manager, PostLikeRepository $likeRepo){
+
+        $user = $this->getUser();
+
+        if (!$user) return $this->json([
+            'code' => 403 ,
+            'message' => 'Unauthorized'],
+            403);
+
+        if ($post->isLikedByUser($user)){
+
+            $like = $likeRepo->findOneBy([
+               'post' => $post,
+                'user' => $user
+            ]);
+
+
+            $manager->remove($like);
+            $manager->flush();
+
+            return $this->json([
+                'code' => 200,
+                'message' => 'like bien supprimé',
+                'likes' => $likeRepo->count(['post' => $post])
+            ], 200 );
+        }
+
+
+        $like = new PostLike();
+
+        $like->setPost($post);
+        $like->getUser($user);
+
+        $manager->persist($like);
+        $manager->flush();
+
+        return $this->json([
+            'code' => 200 ,
+            'message' => 'ca marche super bien !'],
+            200);
+    }
+
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Post;
+use App\Entity\PostLike;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -26,12 +27,25 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create();
-
+        $users = [];
         $user = new User();
         $user->setEmail('user@symfony.com')
             ->setPassword($this->encoder->encodePassword($user, 'password'));
 
         $manager->persist($user);
+
+        $users[]  = $user;
+
+        for ($i = 0; $i< 20; $i++){
+            $user = new User();
+
+            $user->setEmail($faker->email)
+                ->setPassword($this->encoder->encodePassword($user, 'password'));
+
+            $manager->persist($user);
+            $users[]  = $user;
+
+        }
 
         for ($i = 0; $i < 20; $i++) {
             $post = new Post();
@@ -40,6 +54,17 @@ class AppFixtures extends Fixture
                 ->setContent('<p>' . join(',', $faker->paragraphs()) . '</p>');
 
             $manager->persist($post);
+
+            for ($j=0; $j < mt_rand(0,10); $j++){
+                $like = new PostLike();
+
+                $like -> setPost($post)
+                    -> setUser($faker->randomElement($users));
+
+                $manager->persist($like);
+            }
+
+
         }
 
         $manager->flush();
